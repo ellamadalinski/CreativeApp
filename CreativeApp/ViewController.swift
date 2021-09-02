cf59a08891c1880081756f5b07cd948ffc8e01de
Ella Madalinski <ella.madalinski24@d155.org>
2021-09-02 08:11:16 -0500

    layout and starting code

--- CreativeApp/ViewController.swift
+++ CreativeApp/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNumberTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var secondNumberTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var outcomeLabelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func addButtonAction(_ sender: UIButton) {
+        var firstNumText = firstNumberTextFieldOutlet.text!
+        if let one = Int(firstNumText) {
+           
+        }
+        else{
+            outcomeLabelOutlet.text = "ERROR"
+        }
+        
+    }
+    
+    
+    
 }
 
