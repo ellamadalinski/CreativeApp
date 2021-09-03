568b0994395d144acb08565823d97f08f71cd8e0
Ella Madalinski <ella.madalinski24@d155.org>
2021-09-03 08:11:36 -0500

    everything, working on return dismisses keyboard

--- CreativeApp/ViewController.swift
+++ CreativeApp/ViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate{
+    
+    
     
     @IBOutlet weak var firstNumberTextFieldOutlet: UITextField!
     
@@ -15,22 +17,89 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var outcomeLabelOutlet: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        firstNumberTextFieldOutlet.delegate = self
+        secondNumberTextFieldOutlet.delegate = self
     }
 
 
     @IBAction func addButtonAction(_ sender: UIButton) {
-        var firstNumText = firstNumberTextFieldOutlet.text!
-        if let one = Int(firstNumText) {
-           
+        let firstNumText = firstNumberTextFieldOutlet.text!
+        let secondNumText = secondNumberTextFieldOutlet.text!
+        
+        
+        if let numOne = Int(firstNumText)
+        {
+            if let numTwo = Int(secondNumText)
+            {
+                outcomeLabelOutlet.text = "\(numOne + numTwo)"
+            }
+            else{
+                outcomeLabelOutlet.text = "ERROR-NUM2"
+            }
         }
         else{
-            outcomeLabelOutlet.text = "ERROR"
+            outcomeLabelOutlet.text = "ERROR-NUM1"
         }
         
     }
+    
+    
+    @IBAction func multiplyButtonAction(_ sender: UIButton) {
+        
+        let firstNumText = firstNumberTextFieldOutlet.text!
+        let secondNumText = secondNumberTextFieldOutlet.text!
+        
+        if let numOne = Int(firstNumText)
+        {
+            if let numTwo = Int(secondNumText)
+            {
+                outcomeLabelOutlet.text = "\(numOne*numTwo)"
+            }
+            else{
+                outcomeLabelOutlet.text = "ERROR-NUM2"
+            }
+        }
+        else{
+            outcomeLabelOutlet.text = "ERROR-NUM1"
+        }
+    }
+    
+    
+    
+    @IBAction func theoremButtonOutlet(_ sender: UIButton) {
+        
+        let firstNumText = firstNumberTextFieldOutlet.text!
+        let secondNumText = secondNumberTextFieldOutlet.text!
+        
+        if let numOne = Int(firstNumText)
+        {
+            if let numTwo = Int(secondNumText)
+            {
+                let before = (numOne*numOne)+(numTwo*numTwo)
+                
+                outcomeLabelOutlet.text = "\(sqrt(Double(before)))"
+            }
+            else{
+                outcomeLabelOutlet.text = "ERROR-NUM2"
+            }
+        }
+        else{
+            outcomeLabelOutlet.text = "ERROR-NUM1"
+        }
+        
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            firstNumberTextFieldOutlet.resignFirstResponder()
+            secondNumberTextFieldOutlet.resignFirstResponder()
+            return true
+        }
+    }
+    
+    
     
     
     
